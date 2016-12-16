667c2139a133008ee5553b38e223da41b1a181e4
limerentfox <christi@limerentfox.com>
2016-12-16 14:38:30 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  ALL = []
+
+  def initialize(args)
+    @name = args['name']
+    @weight = args['weight']
+    @height = args['height']
+    self.class.all << self
+  end
+
+  def self.all
+    ALL
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def initialize(args)
+    @name = args['name']
+    @type = args['type']
+    @booty = args['booty']
+    self.class.all << self
+  end
+
+  def self.clear
+    self.all.clear
+  end
 end
